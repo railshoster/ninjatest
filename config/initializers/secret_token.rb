4581c38f0a8c82781f8cec09020e12c0bbebf866
Julian Fischer <fischer@enterprise-rails.de>
2012-03-12 16:40:13 +0100

    everything

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ninjatest::Application.config.secret_token = 'c2db6c9c0c0ca893a400eb65bd0a6cefc64e2445c3ace0163962caa9445e1c1fb6c400c2b73f34d9c9623ce84fa891fec6c269cdfb3a44f524b95b0c4271ec52'
